09c967a0bc56058bca9cdbbbecfe35420f438f3c
Sushil Nagi <snagi@sapient.com>
2020-07-19 22:21:55 +0100

    initial version

---
+++ component---src-pages-page-2-js-63818396b3d95a5cd399.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("/lKu"),o=n("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(o.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-63818396b3d95a5cd399.js.map
