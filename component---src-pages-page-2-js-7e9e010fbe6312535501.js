756a430e1d7b2421ad1d8c07437f30d6d74335aa
talktweet <sushil.nagi@gmail.com>
2020-07-20 20:42:36 +0000

    Update from https://github.com/talktweet/blog-site/commit/14df1c045b8381ca63d347ea88865ad148fcf46e)

---
+++ component---src-pages-page-2-js-7e9e010fbe6312535501.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("/lKu"),o=n("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(o.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-7e9e010fbe6312535501.js.map
